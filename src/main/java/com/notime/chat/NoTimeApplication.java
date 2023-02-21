e24f66e4355d78e85bcf43f9baa44120737af2a8
xaxapetov <xaxapetov@yandex.ru>
2023-02-21 17:31:08 +0300

    First Commit

---
+++ src/main/java/com/notime/chat/NoTimeApplication.java
@@ -0,0 +1,13 @@
+package com.notime.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoTimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoTimeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/notime/chat/NoTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notime.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoTimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
